abc20a093d1b0316b4bf4ef5193220f336991f05
Yunus Mustafa Zanzibarwala <85278320+yunuszanzi@users.noreply.github.com>
2025-10-13 16:01:09 +0500

    code push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
-// import ProductList from "@/components/ProductList";
-import  ProductList  from "@/components/ProductList";
+import ProductList from "@/components/ProductList";
 import Image from "next/image";
 
-const Homepage = () => {
+const Homepage = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ category: string }>;
+}) => {
+  const category = (await searchParams).category;
   return (
     <div className="">
       <div className="relative aspect-[3/1] mb-12">
-        <Image src="/featured.png" alt="Feaatured Images" fill></Image>
+        <Image src="/featured.png" alt="Featured Product" fill />
       </div>
-      <ProductList { category: string; params: "homepage" | "products"; }/>
+      <ProductList category={category} params="homepage" />
     </div>
   );
 };
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,47 +1,101 @@
+"use client";
 import { ProductType } from "@/types";
+import { ShoppingCart } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 const ProductCard = ({ product }: { product: ProductType }) => {
+  const [ProductsTypes, setProductTypes] = useState({
+    size: product.sizes[0],
+    color: product.colors[0],
+  });
+
+  const handleProductType = ({
+    type,
+    value,
+  }: {
+    type: "size" | "color";
+    value: string;
+  }) => {
+    setProductTypes((prev) => ({
+      ...prev,
+      [type]: value,
+    }));
+  };
   return (
     <div className="shadow-lg rounded-lg overflow-hidden">
       {/* {Image} */}
       <Link href={`/product/${product.id}`}>
         <div className="relative aspect-[2/3]">
           <Image
-            src={product.images[product.colors[0]]}
+            src={product.images[ProductsTypes.color]}
             alt={product.name}
             fill
             className="object-cover hover:scale-105 transition-all duration-300"
-          ></Image>
+          />
         </div>
       </Link>
-      {"Product Details"}
+      {/* "Product Details" */}
       <div className="flex flex-col gap-4 p-4">
         <h1 className="font-medium">{product.name}</h1>
         <p className="text-sm text-gray-500">{product.shortDescription}</p>
-      </div>
-      {"PRODUCT TYPES"}
-      <div className="flex items-center gap-4 text-xs">
-        {"PRODUCT SIZES"}
-        <div className="flex flex-col gap-1">
-          <span className="text-gray-500">Size</span>
-          <select
-            name="size"
-            id="size"
-            className="ring ring-gray-300 rounded-md px-2 py-1"
-          >
-            {product.sizes.map((size) => (
-              <option key={size} value={size}>
-                {size.toUpperCase()}
-              </option>
-            ))}
-          </select>
+
+        {/* "PRODUCT TYPES" */}
+        <div className="flex items-center gap-4 text-xs">
+          {/* "PRODUCT SIZES" */}
+          <div className="flex flex-col gap-1">
+            <span className="text-gray-500">Size</span>
+            <select
+              name="size"
+              id="size"
+              className="ring ring-gray-300 rounded-md px-2 py-1"
+              onChange={(e) =>
+                handleProductType({ type: "size", value: e.target.value })
+              }
+            >
+              {product.sizes.map((size) => (
+                <option key={size} value={size}>
+                  {size.toUpperCase()}
+                </option>
+              ))}
+            </select>
+          </div>
+          {/* COLORS` */}
+          <div className="flex flex-col gap-1">
+            <span className="text-gray-500">Color</span>
+            <div className="flex items-center gap-2">
+              {product.colors.map((color) => (
+                <div
+                  className={`cursor-pointer border-1 ${
+                    ProductsTypes.color === color
+                      ? "border-gray-400"
+                      : "border-gray-200"
+                  } rounded-full p-[1.2px]`}
+                  key={color}
+                  onClick={() =>
+                    handleProductType({
+                      type: "color",
+                      value: color,
+                    })
+                  }
+                >
+                  <div
+                    className="w-[14px] h-[14px] rounded-full"
+                    style={{ backgroundColor: color }}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
-        {"COLORS"}
-        <div className="flex flex-col gap-1">
-          <span className="text-gray-500">Color</span>
+        {/* PRICE AND ADD TO CART BUTTON */}
+        <div className="flex items-center justify-between">
+          <p className="font-medium">${product.price.toFixed(2)}</p>
+          <button className="ring-1 ring-gray-200 shadow-lg rounded-md px-2 py-1 text-sm cursor-pointer hover:text-white hover:bg-black transition-all duration-300 flex items-center gap-2">
+            <ShoppingCart className="w-4 h-4" />
+            Add to Cart
+          </button>
         </div>
       </div>
     </div>
